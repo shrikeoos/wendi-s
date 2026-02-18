a272d83bdf602c8c79ca06b492e5c975db696c0f
shrikeoos <ngo.romain@gmail.com>
2026-02-18 17:21:20 +0800

    Defined base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/wendi-s/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
